4787310ffb50e555edf7be7ffeb10d965b5085f6
danielmatr <matraimov.daniel@gmail.com>
2022-03-12 22:32:50 +0600

    DELETE, UPDATE, GET

--- controllers/AddController.py
+++ controllers/AddController.py
@@ -4,8 +4,8 @@ class Addcontroller:
     def __init__(self):
         self.products = Products()
 
-    def add(self, name, description, barcode):
-        response = self.products.add(name, description, barcode)
+    def add(self, id, name, description, barcode, price):
+        response = self.products.add(id, name, description, barcode, price)
 
         if response > 0:
             return 'Success'
@@ -16,5 +16,16 @@ class Addcontroller:
         products = self.products.get_all()
         if products:
             return products
+        else:
+            return "Error"
+
+class Updatecontroller:
+    def __init__(self):
+        self.products = Products()
+
+    def update(self, id, name, description, barcode, price):
+        updated_product = self.products.update(id, name, description, barcode, price)
+        if updated_product:
+            return "Success"
         else:
             return "Error"
--- models/Products.py
+++ models/Products.py
@@ -3,29 +3,56 @@ from psycopg2 import Error
 class Products:
     """Connecting to a database"""
     def __init__(self):
-        self.connection = psycopg2.connect(user="mvcbaiel",
-                                           password="19172211",
+        self.connection = psycopg2.connect(user="daniel",
+                                           password="1",
                                            host="localhost",
                                            port="5432",
                                            database="mvc")
         self.cursor = self.connection.cursor()
 
     """Adds new product to the products table"""
-    def add(self, name, description, barcode):
+    def add(self, name, description, barcode, price):
         response = 0
 
         try:
-            self.cursor.execute("INSERT INTO products (name, description, barcode) VALUES (%s, %s, %s)", (name, description, barcode))
+            self.cursor.execute("INSERT INTO products (name, description, barcode, price) VALUES (%s, %s, %s, %s)", (name, description, barcode, price))
             self.connection.commit()
             self.cursor.close()
             self.connection.close()
             response += 1
 
-        except:
-            pass
+        except Exception as e:
+            print(e)
+
         return response
 
     """Gets all products from table"""
     def get_all(self):
         self.cursor.execute("SELECT * FROM products")
         return self.cursor.fetchall()
+
+    def update(self, id, name, description, barcode, price):
+        responce = 0
+        try:
+            self.cursor.execute("UPDATE products SET name = %s, description = %s,barcode = %s, price = %s WHERE id = %s",
+                                (name, description, barcode, price, id))
+            self.connection.commit()
+            self.cursor.close()
+            self.connection.close()
+            responce += 1
+        except:
+            pass
+
+        return responce
+
+    def delete(self, id):
+        self.cursor.execute(
+            "DELETE FROM products WHERE id = %s",
+            (id,))
+        return self.cursor.rowcount
+
+    def get(self, id):
+        self.cursor.execute("SELECT * FROM customers WHERE id = %s", (id,))
+        return self.cursor.fetchone()
+
+
